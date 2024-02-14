1641a6f45b7afd77fb766fb31dcf53a098461467
Passarin <almeida.andre@gmail.com>
2024-02-14 20:06:04 -0300

    hacks

--- Program.cs
+++ Program.cs
@@ -1,32 +1,24 @@
 #pragma warning disable
-using System.Data.SqlClient;
-using System.Data;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Npgsql;
-using System.Text.Json;
-using Microsoft.AspNetCore.Http;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Concurrent;
-
 
 var builder = WebApplication.CreateSlimBuilder(args);
-builder.Services.AddNpgsqlDataSource(@$"
-    User ID=root;Password=998877;
-    Host={Environment.GetEnvironmentVariable("PG_HOST") ?? "localhost"};
-    Port=5432;Database=rinha;Pooling=true;Minimum Pool Size=50;Maximum Pool Size=256;");
+var pgHost = Environment.GetEnvironmentVariable("PG_HOST") ?? "localhost";
+var maxPoolSize = Environment.GetEnvironmentVariable("MAX_POOL_SIZE") ?? "256";
+var connStr = @$"Host={pgHost};Database=rinha;Username=root;Password=998877;Minimum Pool Size=50;Maximum Pool Size={maxPoolSize};";
 
+builder.Services.AddSingleton<NpgsqlDataSource>(new NpgsqlDataSourceBuilder(connStr).Build());
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
-
 builder.Logging.ClearProviders();
 var app = builder.Build();
 app.UseUnprocessableEntityMiddleware();
 
-app.MapPost("/clientes/{id}/transacoes", async (NpgsqlDataSource dataSource, [FromRoute] short id, [FromBody] Transacao t) =>
+app.MapPost("/clientes/{id}/transacoes", 
+    ([FromServices] NpgsqlDataSource dataSource, [FromRoute] short id, [FromBody] Transacao t) =>
 {
     if (id > 5) return Results.NotFound();
 
@@ -34,57 +26,50 @@ app.MapPost("/clientes/{id}/transacoes", async (NpgsqlDataSource dataSource, [Fr
         return Results.UnprocessableEntity();
 
     var limite = new[] { 1000_00, 800_00, 10_000_00, 100_000_00, 5000_00 }[id - 1];
-
+    
     switch (t.tipo)
     {
         case "d":
-            {
-                var res = await dataSource.CreateCommand(@$"
-                    SELECT D({id}::SMALLINT, {t.valor}, '{t.descricao}', {limite})")
-                    .ExecuteScalarAsync();
-                if (res is DBNull) 
-                    return Results.UnprocessableEntity();
-                return Results.Ok(new SaldoLimite((int)res, limite));
-            }
-
+        {
+            var conn = dataSource.OpenConnection();
+            var res = new NpgsqlCommand(@$"SELECT D({id}::SMALLINT, {t.valor}, '{t.descricao}', {limite})", conn).ExecuteScalar();
+            conn.Close();
+            if (res is DBNull) return Results.UnprocessableEntity();
+            return Results.Ok(new SaldoLimite((int)res, limite));
+        }
         case "c":
-                return Results.Ok(new SaldoLimite((int)await dataSource.CreateCommand(@$"
-                    SELECT C({id}::SMALLINT, {t.valor}, '{t.descricao}')")
-                    .ExecuteScalarAsync(), limite));
+        {
+            var conn = dataSource.OpenConnection();
+            var saldo = new NpgsqlCommand(@$"SELECT C({id}::SMALLINT, {t.valor}, '{t.descricao}')", conn).ExecuteScalar();
+            conn.Close();
+            return Results.Ok(new SaldoLimite((int) saldo, limite));
+        }
         default:
             return Results.UnprocessableEntity();
     }
      
 });
-
-app.MapGet("/clientes/{id}/extrato", async (NpgsqlDataSource dataSource, [FromRoute] short id) =>
+app.MapGet("/clientes/{id}/extrato", (NpgsqlDataSource dataSource, [FromRoute] short id) =>
 {
     if (id > 5) return Results.NotFound(); 
 
     var limite = new[] { 1000_00, 800_00, 10_000_00, 100_000_00, 5000_00 }[id - 1];
+    
+    var conn_e = dataSource.OpenConnection();
+    var saldo = new NpgsqlCommand(@$"SELECT saldo FROM cliente WHERE id = {id}", conn_e).ExecuteScalar();
+    conn_e.Close();
 
-    var res = await dataSource.CreateCommand(@$"
-            SELECT valor, tipo, descricao, realizada_em 
-            FROM transacao 
-            WHERE cliente_id = {id}
-            ORDER BY realizada_em DESC LIMIT 10").ExecuteReaderAsync();
+    var conn_c = dataSource.OpenConnection();
+    var res = new NpgsqlCommand(@$"
+            SELECT valor, tipo, descricao, realizada_em FROM transacao WHERE cliente_id = {id}
+            ORDER BY realizada_em DESC LIMIT 10", conn_c).ExecuteReader();
+    var transacoes = Enumerable.Range(0, 10).Select(_ => res.Read()).TakeWhile(hasNext => hasNext)
+            .Select(_ => new Transacao(valor: res.GetInt32(0),tipo: res.GetString(1),
+                descricao: res.GetString(2),realizada_em: res.GetInt64(3))).ToArray();
+    conn_c.Close();
 
-    return Results.Ok(new Extrato(
-        saldo: new Saldo(
-            (int) await dataSource
-            .CreateCommand(@$"SELECT saldo FROM cliente WHERE id = {id}")
-            .ExecuteScalarAsync(), DateTime.Now, limite), 
-        ultimas_transacoes: Enumerable.Range(0, 10)
-            .Select(_ => res.ReadAsync().Result)
-            .TakeWhile(hasNext => hasNext)
-            .Select(_ => new Transacao(
-                valor: res.GetInt32(0),
-                tipo: res.GetString(1),
-                descricao: res.GetString(2),
-                realizada_em: res.GetInt64(3)))
-            .ToArray()));
+    return Results.Ok(new Extrato(new Saldo((int)saldo , DateTime.Now, limite), transacoes));
 });
-
 app.Run($"http://0.0.0.0:{Environment.GetEnvironmentVariable("HTTP_PORT") ?? "80"}");
 
 record TransacaoPayload (int valor, char tipo, string descricao);
